fe0957de4020705300166b7d6e88255e8108c05d
fmoedrit <fmoedrit@users.noreply.github.com>
2014-05-08 14:01:01 +0200

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,13 @@
+setwd("D:/fhtechnikum/bi_specialization/_materials/courses/eda_cw1");
+mydata <- read.table("household_power_consumption.txt", header=TRUE, sep=";", dec=".");
+
+# filtering entries from 01-02-2007 and 02-02-2007
+ind1 <- which(mydata$Date == '1/2/2007');
+ind2 <- which(mydata$Date == '2/2/2007');
+mydata_f <- mydata[ c(ind1, ind2) , ];
+
+# create plot 1: histogramm of global active power
+hist(
+  as.numeric(paste(mydata_f$Global_active_power)), col="red",
+  main="Global Active Power", xlab="Global Active Power (kilowatts)"
+);
